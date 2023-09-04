4a6fbc9dbcabc426f16640ba6bbf79e0d72613e8
Avinash Sharma-18341 <avinash.sharma.cet@gmail.com>
2023-09-04 20:01:04 +0530

    Closures

---
+++ public/advanced/closures/index.js
@@ -0,0 +1,40 @@
+/**
+ * Closures 
+ * Uses of Closures:
+ *  1) Module design pattern
+ *  2) Currying
+ *  3) Function Like once
+ *  4) Memoize
+ *  5) Maintaing state in async world
+ *  6) SetTimeouts
+ *  7) Iterators
+ *  8) and many more.... 
+ * 
+ */
+
+// Example 1
+function x(){
+    var a = 7;
+    function y(){
+        console.log(a);
+    }
+    a = 100;
+    return y;
+}
+var output = x();
+output();
+
+// Example 2
+function p()
+{
+    var b = 100;
+    function q() {
+        var a = 7;
+        function r(){
+            console.log(a,b);
+        }
+        r();
+    }
+    q();
+}
+p();
