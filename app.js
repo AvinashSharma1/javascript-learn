1a54afd40efbba982f1feb0e769cb4c6a0f2ec37
Avinash Sharma-18341 <avinash.sharma.cet@gmail.com>
2023-09-04 20:04:40 +0530

    Learn Javascript from basics to advanced

--- app.js
+++ app.js
@@ -20,9 +20,56 @@ try{
         res.sendFile(__dirname+'/basics/call-apply-bind/index.html');
     });
 
+    app.get('/polyfill/', function (req, res) {
+        res.sendFile(__dirname+'/basics/pollyfill/index.html');
+    });
+
     app.get('/currying/', function (req, res) {
         res.sendFile(__dirname+'/basics/currying/index.html');
     });
+
+    app.get('/debouncing/', function (req, res) {
+        res.sendFile(__dirname+'/basics/debouncing/index.html');
+    });
+   
+    app.get('/throttling/', function (req, res) {
+        res.sendFile(__dirname+'/basics/throttling/index.html');
+    });
+
+    app.get('/event-bubbling-capturing/', function (req, res) {
+        res.sendFile(__dirname+'/basics/event-bubbling-capturing/index.html');
+    });
+    app.get('/event-deligation/', function (req, res) {
+        res.sendFile(__dirname+'/basics/event-deligation/index.html');
+    });
+
+    app.get('/prototype/', function (req, res) {
+        res.sendFile(__dirname+'/basics/prototype/index.html');
+    });
+
+    /**
+     * Advanced start
+     */
+    app.get('/call-stack/', function (req, res) {
+        res.sendFile(__dirname+'/public/advanced/call-stack/index.html');
+    });
+    app.get('/lexical-environment/', function (req, res) {
+        res.sendFile(__dirname+'/public/advanced/lexical-environment/index.html');
+    });
+
+    app.get('/let-const/', function (req, res) {
+        res.sendFile(__dirname+'/public/advanced/let-const/index.html');
+    });
+    
+    app.get('/block-scope-shadowing/', function (req, res) {
+        res.sendFile(__dirname+'/public/advanced/block-scope-shadowing/index.html');
+    });
+
+    app.get('/closures/', function (req, res) {
+        res.sendFile(__dirname+'/public/advanced/closures/index.html');
+    });
+
+
 }
 catch(error){
     console.log(error);
